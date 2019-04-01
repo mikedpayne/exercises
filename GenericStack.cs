e1cca3d445f0f9142975f74861342be74e76071f
mikedpayne <mikedpayne@gmail.com>
2019-03-31 22:06:41 -0400

    Add files via upload

---
+++ GenericStack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelearningConsole
+{
+    class GenericStack<T>
+    {
+        List<T> _stack = new List<T>();
+
+        public void push(T obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException();
+
+            _stack.Add(obj);
+        }
+
+        public T pop()
+        {
+            if (_stack.Count == 0)
+                throw new InvalidOperationException();
+
+            T obj = _stack[_stack.Count - 1];
+            _stack.Remove(obj);
+            return obj;
+        }
+    }
+}
---
+++ Stopwatch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelearningConsole
+{
+    class Stopwatch
+    {
+        private bool isRunning = false;
+        public DateTime? StartTime { get; private set;  }
+        public DateTime? StopTime { get; private set; }
+
+        TimeSpan? Duration
+        {
+            get
+            {
+                if (StartTime != null && StopTime != null)
+                    return StopTime - StartTime;
+                else if (StartTime != null)
+                    return DateTime.Now - StartTime;
+                else
+                    return null;
+            }
+        }
+
+        public void Start()
+        {
+            if (isRunning)
+                throw new InvalidOperationException("Stopwatch already running.");
+
+            StartTime = DateTime.Now;
+            StopTime = null;
+            isRunning = true;
+        }
+
+        public void Stop()
+        {
+            if (!isRunning)
+                throw new InvalidOperationException("Stopwatch not running.");
+
+            StopTime = DateTime.Now;
+            isRunning = false;
+        }
+    }
+}
