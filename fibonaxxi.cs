bbbefb2a1fde49bcfbfa8a6eb8e13605c3c048de
mikedpayne <mikedpayne@gmail.com>
2022-11-13 16:08:45 -0500

    Create fibonaxxi.cs

---
+++ fibonaxxi.cs
@@ -0,0 +1,28 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int last = 0;
+		int next = 1;
+		
+		Console.WriteLine("Stop at what number? ");
+		int stop = Convert.ToInt32(Console.ReadLine());
+		
+		Console.Write(last + ", ");
+		next += last;
+		
+		while (next <= stop)
+		{
+			Console.Write(next);
+			if (next < stop)
+			{
+				Console.Write(", ");
+			}
+			int a = next;
+			next += last;
+			last = a;
+		}
+	}
+}
